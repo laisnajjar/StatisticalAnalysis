b6e9f83169727e905fca3ab7ab4a0380c649c401
laisnajjar <laisnajj@umich.edu>
2023-07-26 00:52:47 -0500

    cleaned repo & updated frequency charts + csv

--- freq.py
+++ freq.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('MasaDubaiFull.csv')
+df = pd.read_csv('Data_Sets/dubai_data.csv')
 
 numeric_columns = df.select_dtypes(include=['int64', 'float64']).columns
 
@@ -13,7 +13,7 @@ for column in numeric_columns:
     except KeyError:
         pass
 
-freq_df.to_csv('FrequencyTable_MasaDubaiFull.csv')
+freq_df.to_csv('FrequencyTable_Dubai.csv')
 
 # unstack the DataFrame
 unstacked_df = freq_df.unstack().reset_index()
@@ -22,9 +22,9 @@ unstacked_df.columns = ['Variable', 'Value', 'Frequency']
 # plot bar graph
 plt.figure(figsize=(10, 5))  # adjust size as needed
 sns.barplot(x='Variable', y='Frequency', hue='Value', data=unstacked_df)
-plt.title('Frequency of 0 and 1 in Variables')
+plt.title('Frequency of Variables in Dubai Sample')
 plt.xlabel('Variables')
 plt.ylabel('Frequency')
 plt.xticks(rotation=90)  # rotate x-axis labels if they're long
 plt.tight_layout()  # adjust layout for better fit
-plt.savefig('FreqBar_MasaDubaiFull.png')
+plt.savefig('FreqBar_Dubai.png')
--- log.py
+++ log.py
@@ -2,20 +2,42 @@
 import pandas as pd
 import statsmodels.api as sm
 #--------Saudi Sample----------------
-saudi_data = pd.read_csv('saudi_data.csv') # Load the data
+saudi_data = pd.read_csv('Data_Sets/saudi_data.csv') # Load the data
 y = saudi_data['Orientalism'] # Define the dependent variable (Orientalism) and the independent variable
 X = saudi_data['Place']  
 X = sm.add_constant(X) # Add a constant to the independent variable
 model = sm.Logit(y, X) # Run the logistic regression
-result = model.fit() # Fit the model
+result1 = model.fit() # Fit the model
 print("Saudi Sample Logistics Regression Results:")
-print(result.summary()) # Print the summary of the regressionS
+print(result1.summary()) # Print the summary of the regressionS
 #--------Dubai Sample----------------
-dubai_data = pd.read_csv('dubai_data.csv')
+dubai_data = pd.read_csv('Data_Sets/dubai_data.csv')
 y = dubai_data['Orientalism']
 X = dubai_data['Place']
 X = sm.add_constant(X)
 model = sm.Logit(y, X)
-result = model.fit()
+result2 = model.fit()
 print("Dubai Sample Logistics Regression Results:")
-print(result.summary())
+print(result2.summary())
+
+# Convert the summary results to text
+result1_text = result1.summary().tables[1].as_text()
+# Split the text by newline character to create a list of rows
+result1_rows = result1_text.split('\n')
+# Split each row by its spaces to create a list of columns
+result1_data = [row.split() for row in result1_rows]
+# Convert the list of columns to a DataFrame
+result1_df = pd.DataFrame(result1_data)
+# Write the DataFrame to a CSV file
+result1_df.to_csv('logistic_regression_saudi.csv', index=False)
+
+# Convert the summary results to text
+result2_text = result2.summary().tables[1].as_text()
+# Spli2 the text by n2wline character to create a list of rows
+result2_rows = result2_text.split('\n')
+# Spli2 each row by its spaces to create a list of columns
+result2_data = [row.split() for row in result2_rows]
+# Conv2rt the list of columns to a DataFrame
+result2_df = pd.DataFrame(result2_data)
+# Writ2 the DataFrame to a CSV file
+result2_df.to_csv('logistic_regression_dubai.csv', index=False)
